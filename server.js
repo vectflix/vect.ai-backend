c5067f78be3260b96f0c0486a0737068f4c8e3ed
vectflix <malcolmotieno69@gmail.com>
2026-02-28 01:53:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post("/generate", async (req, res) => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "mixtral-8x7b-32768",
+        model: "llama3-8b-8192",
         messages: [
           { role: "system", content: "You are an autonomous website builder AI." },
           { role: "user", content: prompt }
